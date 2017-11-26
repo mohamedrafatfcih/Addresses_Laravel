a476ebe1ff4cb263b290c6470f10de1d8565569e
mohamedrafatfcih <mohamedrafatfcih@gmail.com>
2017-11-26 12:39:02 +0200

    DB Migrations

---
+++ database/migrations/2017_11_26_075415_create_tables.php
@@ -0,0 +1,105 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        // countries  table
+        Schema::create('countries', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('country_name');
+            $table->integer('population');
+            $table->float('area');
+            $table->string('main_lang');
+            $table->timestamps();
+        });
+
+        // states table
+
+        Schema::create('states', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('state_name');
+            $table->integer('country_id')->unsigned();
+            $table->foreign('country_id')->references('id')->on('countries');
+            $table->timestamps();
+        });
+
+        // cities table
+        Schema::create('cities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('city_name');
+            $table->integer('state_id')->unsigned();
+            $table->foreign('state_id')->references('id')->on('states');
+            $table->timestamps();
+        });
+
+        /*
+         *
+         *    Schema::create('translations_states', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('translated_to');
+            $table->string('trans_lang');
+            $table->string('source_type');
+            $table->integer('source_id')->unsigned();
+            $table->foreign('source_id','translations_country_id_foreign')->references('id')->on('countries');
+            $table->foreign('source_id','translations_state_id_foreign')->references('id')->on('states');
+            $table->foreign('source_id','translations_city_id_foreign')->references('id')->on('cities');
+            $table->timestamps();
+        });
+         */
+
+        // translation tables
+
+        Schema::create('translations_countries', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('translated_to');
+            $table->string('trans_lang');
+            $table->integer('source_id')->unsigned();
+            $table->foreign('source_id')->references('id')->on('countries');
+            $table->timestamps();
+        });
+
+        Schema::create('translations_states', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('translated_to');
+            $table->string('trans_lang');
+            $table->integer('source_id')->unsigned();
+            $table->foreign('source_id')->references('id')->on('states');
+            $table->timestamps();
+        });
+
+
+        Schema::create('translations_cities', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('translated_to');
+            $table->string('trans_lang');
+            $table->integer('source_id')->unsigned();
+            $table->foreign('source_id')->references('id')->on('cities');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+        Schema::dropIfExists('states');
+        Schema::dropIfExists('cities');
+    }
+}
