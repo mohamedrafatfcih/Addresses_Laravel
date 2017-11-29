ad283767cb20b8629acabf83623c61c2920efd5b
mohamedrafatfcih <mohamedrafatfcih@gmail.com>
2017-11-29 12:50:54 +0200

    Edit DB Migration for Countries Prefixes and Models

--- app/LocalNumberPrefix.php
+++ app/LocalNumberPrefix.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class LocalNumberPrefix extends Model
 {
-    protected $table = "";
+    protected $table = "country_number_perfixs";
 
     public function country(){
-        return $this->belongsTo(Country::class);
+        return $this->belongsTo(Country::class,'country_id','id');
     }
 }
